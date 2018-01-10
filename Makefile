812684a8ab1be1b7b2655f2917052ca8c28bb9c9
Jaroslav Loebl <jaroslavloebl@gmail.com>
2018-01-10 18:25:25 +0100

    add pipenv and build task

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 test:
 	python -m pytest -s -v
+
+develop:
+	python setup.py develop
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='pycgp',
+    version='0.2',
+    description='CGP library',
+    author='Jaroslav Loebl',
+    author_email='jaroslavloebl@gmail.com',
+    packages=['pycgp'])
