02eeb7c106ab7013a0e03d204c2af45222647a04
Jaroslav Loebl <jaroslavloebl@gmail.com>
2018-03-07 17:02:07 +0100

    stats in the result

--- pycgp/evolution.py
+++ pycgp/evolution.py
@@ -2,68 +2,77 @@ from pycgp.individual_builder import IndividualBuilder
 from pycgp.gems import JewelleryBox, GemSingleGene, MatchPMStrategy
 from pycgp.selection import truncation_selection
 from pycgp.mutation import point_mutation, probabilistic_mutation
-from pycgp.counter import Counter
 
 import statistics
+import pdb
 
 from pycgp.individual import Individual
 
+def initialize_stats_dict(tuples_with_init):
+    stats = {}
+    for key, init in tuples_with_init:
+        stats[key] = init
+    return stats
 
 def evolution(cgp_params, ev_params, X, y, verbose=False):
     """
-    ev_params fields:
-    - cost_func: callable, cost function, recieving target vector and computed vector as parameters
-    - target_fitness: int, target fitness upon which reaching terminate the evolution
-    - gems: bool, whether to use gems extension in evolution, defaults to False
-    - j_box_size: int, maximum capacity of jewellery box, defaults to 5
-    - max_evaluations: int, maximum number of cost function evaluations, upon which evolution will be stopped, defaults to 5000
-    - pop: int, number of individuals in population, defaults to 5
-    - selection: callable, selection operator
-    - mutation: callable, mutation operator
-    - match_strategy
-    - gem_type
-    - mutation_probability: float (0,1), probability of mutating a single gene, used only with probablisitic mutation
+    Performs the evolutionary search defined by ev_params for CGP system defined
+    by cgp_params on input data X and target data y
     """
     builder = IndividualBuilder(cgp_params)
     apply_gem = ev_params.gems_box_size
     target_fitness_is_set = ev_params.target_fitness is not None
-
+    j_box = JewelleryBox(ev_params.gem_match_strategy(), max_size=apply_gem)
+    stats = initialize_stats_dict([
+        ('mean_of_generation', []),
+        ('best_of_generation', []),
+        ('best', None),
+        ('gem_better_after', 0),
+        ('gem_worse_after', 0)
+    ]) 
+    evaluations_counter = 0
 
     population = [builder.build() for _ in range(0, ev_params.population_size)]
-    evaluations_counter = 0
-    Counter.get().dict['g_better'] = 0
-    Counter.get().dict['g_worse'] = 0
-    Counter.get().dict['mean'] = []
-    Counter.get().dict['best'] = []
-    Counter.get().dict['g_better_fitness'] = []
-    Counter.get().dict['remove_gem'] = 0
-    Counter.get().dict['best_individual'] = []
-    Counter.get().dict['not_equal_a'] = 0
-    Counter.get().dict['not_equal_o'] = 0
 
-    j_box = JewelleryBox(ev_params.gem_match_strategy(), max_size=apply_gem)
+    stats['best'] = population[0] # doesn't matter which one
 
     for individual in population:
         output = individual.execute(X)
         individual.fitness = ev_params.cost_function(y, output)
         evaluations_counter += 1
    
-    Counter.get().dict['gens'] = 0
-    Counter.get().dict['g_same_as_parent'] = 0
     gens = 0
+
     while evaluations_counter < ev_params.max_evaluations:
         gens += 1
-        # store mean of population and best
-        Counter.get().dict['mean'].append(statistics.mean(
+
+
+
+        # store mean of population and best individual
+        stats['mean_of_generation'].append(statistics.mean(
             [x.fitness for x in population]
         ))
-        Counter.get().dict['best'].append(min(
-            population, key=lambda x: x.fitness
-        ).fitness)
 
+        # store the best individual of population
+        stats['best_of_generation'].append(min(
+            enumerate(population), key=lambda x: x[1].fitness
+            )[1]
+        )
+
+        if stats['best_of_generation'][-1].fitness < stats['best'].fitness:
+            stats['best'] = stats['best_of_generation'][-1]
+
+        if verbose and evaluations_counter % 1000 == 0:
+            print('Number of evals: {}, mean: {}, best: {}'.format(
+                evaluations_counter, stats['mean_of_generation'][-1],
+                stats['best'].fitness
+            ))
 
         parent = ev_params.selection(population, 1)[0]
-        Counter.get().dict['best_individual'].append(parent)
+
+        # while this works in case of truncation selection with one parent
+        # it wouldn't work with any other type of selection operators
+        # stats['best_of_generation'].append(parent)
 
         if target_fitness_is_set and (parent.fitness <= ev_params.target_fitness):
             break
@@ -89,7 +98,6 @@ def evolution(cgp_params, ev_params, X, y, verbose=False):
 
         for index, (individual, m_index) in enumerate(zip(population, m_indices)):
             if individual.fitness < parent.fitness:
-                Counter.get().dict['g_better_fitness'].append(individual.fitness - parent.fitness)
                 j_box.add(
                     ev_params.gem_match_strategy.associated_gem_type(
                         individual, parent, m_index))
@@ -99,10 +107,7 @@ def evolution(cgp_params, ev_params, X, y, verbose=False):
                 if matching_gem is not None:
                     new_individual = matching_gem.apply(individual)
 
-                    if new_individual is None:
-                        Counter.get().dict['g_same_as_parent'] += 1
-                    else:
-                        # if gem exceeds 30 uses, remove
+                    if new_individual is not None: 
                         if ev_params.gem_expire and matching_gem.n_uses >= ev_params.gem_expire:
                             j_box.remove(matching_gem)
 
@@ -110,17 +115,16 @@ def evolution(cgp_params, ev_params, X, y, verbose=False):
                         evaluations_counter += 1 
                         
                         if new_individual.fitness < individual.fitness:
-                            Counter.get().dict['g_better'] += 1
+                            stats['gem_better_after'] += 1
                             population[index] = new_individual
                         else:
-                            Counter.get().dict['g_worse'] += 1
+                            stats['gem_worse_after'] += 1
                     
                         population[index] = new_individual
 
 
         population = population + [parent]
 
-    Counter.get().dict['gens'] = gens
     s_pop = sorted(population, key=lambda x: x.fitness)
 
     if verbose:
@@ -131,8 +135,9 @@ def evolution(cgp_params, ev_params, X, y, verbose=False):
 
     
     results = {
-            'evals': evaluations_counter,
-            'final': s_pop
+            'n_evals': evaluations_counter,
+            'final': s_pop,
+            'stats': stats
     }
 
     return results
