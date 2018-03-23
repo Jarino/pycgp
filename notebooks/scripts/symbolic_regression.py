9111eafdda3c5dbb022598ee2f6e3bc9bf893760
Jaroslav Loebl <jaroslavloebl@gmail.com>
2018-03-23 13:39:54 +0100

    pypy compatible symbolic regression and results interpretation

--- notebooks/scripts/symbolic_regression.py
+++ notebooks/scripts/symbolic_regression.py
@@ -1,15 +1,20 @@
-from pycgp.benchmarks.symbolic import PARAMS, EV_PARAMS, X, y, target_function
+import random
+from time import time
+import sys
+import os
+import pickle
+from functools import reduce
+from operator import add
+from copy import deepcopy
+
+from pycgp.benchmarks.symbolic import * 
 from pycgp import Params, EvParams
 from pycgp.evolution import evolution
 from pycgp.counter import Counter
-import random
-import numpy as np
-from time import time
-import pandas as pd
-import sys
-from functools import reduce
-from operator import add
+
+
 experiment_count = 0
+
 
 def get_data(gems):
     return [(
@@ -20,10 +25,8 @@ def get_data(gems):
         x.match_probability
         ) for x in gems]    
     
-    columns = ['n_uses', 'match_checks', 'match_count', 'gene_possible_values', 'match_probability']
-    data.columns = columns
-    data['empiric'] = data.iloc[:,2]/data.iloc[:,1]
-    return data
+    #columns = ['n_uses', 'match_checks', 'match_count', 'gene_possible_values', 'match_probability']
+    #return data
 
 def run_experiment(params, ev_params, x, y):
     global experiment_count
@@ -31,49 +34,56 @@ def run_experiment(params, ev_params, x, y):
 
     print('Experiment #{}'.format(experiment_count))
     start = time()
-    gstat = []
-    n_better = []
-    n_worse = []
-    n_same = []
-    best_fitness = []
-    mean_fitness = []
-    std_fitness = []
+    # gstat = []
+    # n_better = []
+    # n_worse = []
+    # n_same = []
+    # best_fitness = []
+    # mean_fitness = []
+    # std_fitness = []
+    exp_stats = []
     for i in range(0, 20):
         print(i, end=', ')
 
         result = evolution(params, ev_params, X, y)
 
         stats = result['stats']
-        best_fitness.append([x.fitness for x in stats['best_of_generation']])
-        mean_fitness.append(stats['mean_of_generation'])
-        std_fitness.append(stats['std_of_generation'])
-        n_better.append(stats['gem_better_after'])
-        n_worse.append(stats['gem_worse_after'])
-        # gstat.append(get_data(result['gem_stats']))
+        stats['best_fitness'] = [x.fitness for x in stats['best_of_generation']]
+        del stats['best_of_generation']
+        stats['number_of_evals'] = result['n_evals']
+        stats['gem_data'] = result['gem_stats']
+        exp_stats.append(deepcopy(stats))
+
+        # best_fitness.append([x.fitness for x in stats['best_of_generation']])
+        # mean_fitness.append(stats['mean_of_generation'])
+        # std_fitness.append(stats['std_of_generation'])
+        # n_better.append(stats['gem_better_after'])
+        # n_worse.append(stats['gem_worse_after'])
+        #gstat.append(get_data(result['gem_stats']))
 
 
     # make the best_fitness and mean_fitness lists the lists with same length
-    for measurement in [best_fitness, mean_fitness]:
-        longest = max([len(x) for x in measurement]) # since we know its only 2D its faster than np.max
-        for l in measurement:
-            extension = [l[-1]] * (longest - len(l))
-            l.extend(extension)
+    # for measurement in [best_fitness, mean_fitness]:
+    #     longest = max([len(x) for x in measurement]) # since we know its only 2D its faster than np.max
+    #     for l in measurement:
+    #         extension = [l[-1]] * (longest - len(l))
+    #         l.extend(extension)
 
-    iob = np.unravel_index(np.argmin(best_fitness), np.array(best_fitness).shape)    
-    results = [
-        np.min(best_fitness), # best fitness of all
-        mean_fitness[iob[0]][iob[1]], # best generation mean
-        std_fitness[iob[0]][iob[1]], # std generation mean
-        np.sum(n_better), # total number of better after gem
-        np.mean(n_better), # averge number of better after gem
-        np.sum(n_worse), # total number of worse after gem
-        np.mean(n_worse) # average number of worse after gem
-    ]
+    # iob = np.unravel_index(np.argmin(best_fitness), np.array(best_fitness).shape)    
+    # results = [
+    #     np.min(best_fitness), # best fitness of all
+    #     mean_fitness[iob[0]][iob[1]], # best generation mean
+    #     std_fitness[iob[0]][iob[1]], # std generation mean
+    #     np.sum(n_better), # total number of better after gem
+    #     np.mean(n_better), # averge number of better after gem
+    #     np.sum(n_worse), # total number of worse after gem
+    #     np.mean(n_worse) # average number of worse after gem
+    # ]
     end = time()
     print('wall time {}'.format(end-start))
 
     # avg_gstat = reduce(add, gstat)/len(gstat) 
-    return results, np.mean(best_fitness, axis=0), np.mean(mean_fitness, axis=0) #, avg_gstat
+    return exp_stats#, np.mean(best_fitness, axis=0), np.mean(mean_fitness, axis=0) #, avg_gstat
 
 columns = [
     'mutation',
@@ -89,7 +99,7 @@ columns = [
     'worse_gems_mean'
 ]
 
-data = pd.DataFrame(columns=columns)
+#data = pd.DataFrame(columns=columns)
 
 # params to change
 # mutation, gems, expire, cols,
@@ -103,37 +113,47 @@ mutations = [
         (single_mutation, MatchByActiveStrategy),
         (probabilistic_mutation, MatchByActiveStrategy)]
 
+output_folder = 'symbolic_out_pypy'
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
 for mutation, strategy in mutations:
     for gems in [0, 5, 10]:
         ev_params = EvParams(
-            EV_PARAMS['cost_func'],
+            cost_func,
             target_fitness=0,
             gems_box_size=gems,
             gem_match_strategy=strategy,
-            mutation=mutation)
+            mutation=mutation,
+            gem_expire=30)
         for n_cols in [10, 50, 100]:
             params = Params(n_columns=n_cols, n_inputs=1, 
                 n_outputs=1,
-                funset=PARAMS['funset'])
+                funset=funset)
 
             row = [mutation.__name__, strategy.__name__, gems, n_cols]
 
             random.seed(1)
 
             filename = f'{mutation.__name__}-{strategy.__name__}-gems{gems}-n_cols{n_cols}.csv'
+
             print(filename)
-            gem_data, best_fitness, mean_fitness = run_experiment(params, ev_params, X, y)
 
-            row += gem_data
+            result = run_experiment(params, ev_params, X, y)
 
-            data.loc[experiment_count] = row
+            with open(os.path.join(output_folder, filename), 'wb') as f:
+                pickle.dump(result, f)
 
-            filename = f'{mutation.__name__}-{strategy.__name__}-gems{gems}-n_cols{n_cols}.csv'
-            fitness_data = pd.DataFrame()
-            fitness_data['best_fitness'] = best_fitness
-            fitness_data['mean_fitness'] = mean_fitness
-            fitness_data.to_csv(filename)
 
-            # gstat.to_csv(f'gstats-{filename}')
+            # row += gem_data
 
-data.to_csv('sym_res_active.csv', sep=',')
+            # data.loc[experiment_count] = row
+
+            # filename = f'{mutation.__name__}-{strategy.__name__}-gems{gems}-n_cols{n_cols}.csv'
+            # fitness_data = pd.DataFrame()
+            # fitness_data['best_fitness'] = best_fitness
+            # fitness_data['mean_fitness'] = mean_fitness
+            # fitness_data.to_csv(filename)
+
+
+#data.to_csv('sym_res_active.csv', sep=',')
